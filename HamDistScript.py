950171190c04f14a07f7268d47fc2e8e711e9c09
Mike <igor_gantman@epam.com>
2019-10-30 14:22:42 +0300

    remove syntax shugar

--- HamDistScript.py
+++ HamDistScript.py
@@ -4,12 +4,10 @@ def ham_dist_func(x, y):
     yBinStr = bin(y)[2:]
     xBinStr32 = xBinStr.zfill(32)
     yBinStr32 = yBinStr.zfill(32)
-    i = 0
     hamDist = 0
-    while i < 32:
+    for i in range(32):
         if xBinStr32[i] != yBinStr32[i]:
-            hamDist = hamDist + 1
-        i = i + 1
+            hamDist += 1
     return hamDist
 
 
